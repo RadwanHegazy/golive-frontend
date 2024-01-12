7b292d3b366943b32cfd42ab91f82c0c1701abd2
RadwanHegazy <radwangaber22@gmail.com>
2024-01-12 19:15:39 +0200

    add some code

--- app/views/room/create.py
+++ app/views/room/create.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from globals.decorators import login_required
-
+from globals.request_manager import Action
+from frontend.settings import MAIN_URL
 
 @login_required
 def CreateRoom (request) : 
-    return render(request,'room-admin.html')
+
+    context = {}
+    if request.method == "POST" : 
+        data = {
+            'title' : request.POST.get('title',None)
+        }
+
+        user = request.COOKIES.get('user')
+
+        headers = {'Authorization':f"Bearer {user}"}
+
+        action = Action(
+            url = MAIN_URL + '/room/create/',
+            data = data,
+            headers=headers
+        )
+
+        action.post()
+        
+        if action.is_valid() :
+            room_id = action.json_data()['id'] 
+            return redirect('room', room_id)
+        
+        context['error'] = "يوجد خطأ اثناء انشاء البث المباشر"
+
+    return render(request,'room-create.html',context)
 
 
--- app/views/room/delete.py
+++ app/views/room/delete.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from globals.decorators import login_required
-
+from frontend.settings import MAIN_URL
+import requests
 
 @login_required
 def DeleteRoom (request, room_id) : 
-    return HttpResponse("DeleteRoom")
+
+
+    
+    user = request.COOKIES.get('user')
+    req = requests.delete(
+        url = f"http://127.0.0.1:4444/room/delete/{room_id}/",
+        headers = {'Authorization':f"Bearer {user}"},
+    )
+
+    print(req.json())
+
+    return redirect('rooms')
 
 
